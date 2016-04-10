d78f59e8a488ecfefe87e45a7807e839165d6db9
Christof Buechi <christof.buechi@gmail.com>
2016-04-11 00:12:29 +0200

    adding swagger

--- src/main/java/bookmarks/Application.java
+++ src/main/java/bookmarks/Application.java
@@ -1,9 +1,8 @@
 // tag::runner[]
 package bookmarks;
 
-import java.util.Arrays;
-import java.util.Collection;
-
+import com.fasterxml.classmate.TypeResolver;
+import org.joda.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,38 +13,62 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.schema.WildcardType;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static springfox.documentation.schema.AlternateTypeRules.newRule;
+
 
 @Configuration
+@EnableSwagger2
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
 
-	@Bean
-	CommandLineRunner init(AccountRepository accountRepository,
-			BookmarkRepository bookmarkRepository) {
-		return (evt) -> Arrays.asList(
-				"jhoeller,dsyer,pwebb,ogierke,rwinch,mfisher,mpollack,jlong".split(","))
-				.forEach(
-						a -> {
-							Account account = accountRepository.save(new Account(a,
-									"password"));
-							bookmarkRepository.save(new Bookmark(account,
-									"http://bookmark.com/1/" + a, "A description"));
-							bookmarkRepository.save(new Bookmark(account,
-									"http://bookmark.com/2/" + a, "A description"));
-						});
-	}
+    @Autowired
+    private TypeResolver typeResolver;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Docket petApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build().pathMapping("/")
+                .directModelSubstitute(LocalDate.class, String.class).genericModelSubstitutes(ResponseEntity.class)
+                .alternateTypeRules(newRule(typeResolver.resolve(DeferredResult.class, typeResolver.resolve(ResponseEntity.class, WildcardType.class)), typeResolver.resolve(WildcardType.class)))
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET, newArrayList(new ResponseMessageBuilder().code(500).message("500 message").responseModel(new ModelRef("Error")).build()));
+    }
+
+    @Bean
+    CommandLineRunner init(AccountRepository accountRepository,
+                           BookmarkRepository bookmarkRepository) {
+        return (evt) -> Arrays.asList(
+                "jhoeller,dsyer,pwebb,ogierke,rwinch,mfisher,mpollack,jlong".split(","))
+                .forEach(
+                        a -> {
+                            Account account = accountRepository.save(new Account(a,
+                                    "password"));
+                            bookmarkRepository.save(new Bookmark(account,
+                                    "http://bookmark.com/1/" + a, "A description"));
+                            bookmarkRepository.save(new Bookmark(account,
+                                    "http://bookmark.com/2/" + a, "A description"));
+                        });
+    }
 }
 // end::runner[]
 
@@ -53,57 +76,57 @@ public class Application {
 @RequestMapping("/{userId}/bookmarks")
 class BookmarkRestController {
 
-	private final BookmarkRepository bookmarkRepository;
+    private final BookmarkRepository bookmarkRepository;
 
-	private final AccountRepository accountRepository;
+    private final AccountRepository accountRepository;
 
-	@RequestMapping(method = RequestMethod.POST)
-	ResponseEntity<?> add(@PathVariable String userId, @RequestBody Bookmark input) {
-		this.validateUser(userId);
-		return this.accountRepository
-				.findByUsername(userId)
-				.map(account -> {
-					Bookmark result = bookmarkRepository.save(new Bookmark(account,
-							input.uri, input.description));
+    @Autowired
+    BookmarkRestController(BookmarkRepository bookmarkRepository,
+                           AccountRepository accountRepository) {
+        this.bookmarkRepository = bookmarkRepository;
+        this.accountRepository = accountRepository;
+    }
 
-					HttpHeaders httpHeaders = new HttpHeaders();
-					httpHeaders.setLocation(ServletUriComponentsBuilder
-							.fromCurrentRequest().path("/{id}")
-							.buildAndExpand(result.getId()).toUri());
-					return new ResponseEntity<>(null, httpHeaders, HttpStatus.CREATED);
-				}).get();
+    @RequestMapping(method = RequestMethod.POST)
+    ResponseEntity<?> add(@PathVariable String userId, @RequestBody Bookmark input) {
+        this.validateUser(userId);
+        return this.accountRepository
+                .findByUsername(userId)
+                .map(account -> {
+                    Bookmark result = bookmarkRepository.save(new Bookmark(account,
+                            input.uri, input.description));
 
-	}
+                    HttpHeaders httpHeaders = new HttpHeaders();
+                    httpHeaders.setLocation(ServletUriComponentsBuilder
+                            .fromCurrentRequest().path("/{id}")
+                            .buildAndExpand(result.getId()).toUri());
+                    return new ResponseEntity<>(null, httpHeaders, HttpStatus.CREATED);
+                }).get();
 
-	@RequestMapping(value = "/{bookmarkId}", method = RequestMethod.GET)
-	Bookmark readBookmark(@PathVariable String userId, @PathVariable Long bookmarkId) {
-		this.validateUser(userId);
-		return this.bookmarkRepository.findOne(bookmarkId);
-	}
+    }
 
-	@RequestMapping(method = RequestMethod.GET)
-	Collection<Bookmark> readBookmarks(@PathVariable String userId) {
-		this.validateUser(userId);
-		return this.bookmarkRepository.findByAccountUsername(userId);
-	}
+    @RequestMapping(value = "/{bookmarkId}", method = RequestMethod.GET)
+    Bookmark readBookmark(@PathVariable String userId, @PathVariable Long bookmarkId) {
+        this.validateUser(userId);
+        return this.bookmarkRepository.findOne(bookmarkId);
+    }
 
-	@Autowired
-	BookmarkRestController(BookmarkRepository bookmarkRepository,
-			AccountRepository accountRepository) {
-		this.bookmarkRepository = bookmarkRepository;
-		this.accountRepository = accountRepository;
-	}
+    @RequestMapping(method = RequestMethod.GET)
+    Collection<Bookmark> readBookmarks(@PathVariable String userId) {
+        this.validateUser(userId);
+        return this.bookmarkRepository.findByAccountUsername(userId);
+    }
 
-	private void validateUser(String userId) {
-		this.accountRepository.findByUsername(userId).orElseThrow(
-				() -> new UserNotFoundException(userId));
-	}
+    private void validateUser(String userId) {
+        this.accountRepository.findByUsername(userId).orElseThrow(
+                () -> new UserNotFoundException(userId));
+    }
 }
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 class UserNotFoundException extends RuntimeException {
 
-	public UserNotFoundException(String userId) {
-		super("could not find user '" + userId + "'.");
-	}
+    public UserNotFoundException(String userId) {
+        super("could not find user '" + userId + "'.");
+    }
 }
